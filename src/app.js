23a0df9fece030dc5adaa81528811a60304d9feb
Sblaise123 <127683538+Sblaise123@users.noreply.github.com>
2023-05-23 23:05:11 +0000

    RandomCardGene

--- src/app.js
+++ src/app.js
@@ -1,11 +1,33 @@
-/* eslint-disable */
-import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+window.onload = () => {
+  document.querySelector(".card").classList.add("heart");
+  setTimeout(() => {
+    document.querySelector(".card").classList.remove("heart");
+    document.querySelector(".card").classList.add("spade");
+  }, 1000);
+};
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+let generateRandomNumber;
+() => {
+  let numb = Math.floor(Math.random() * 12);
+  let numbers = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  let suit = ["diamond", "spade", "heart", "club"];
+
+  let indexNumbers = Math.floor(Math.random() * numbers.length);
+  let indexSuit = Math.floor(Math.random() * suit.length);
 };
